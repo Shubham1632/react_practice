0a5923f6f08f9df9f235355fdff8f2b352dd57fe
shubham-gangurde_psl <shubham_gangurde@persistent.com>
2022-11-23 12:54:37 +0530

    used hooks (useState) to fetch the realtime data

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
-
+import Time from "./time";
 
 
 
 export default function App(){
     return(
         <>
+
             <Header />
+            <Time />
             <Content />
             <Footer />
         </>
---
+++ src/time.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+export default function Time(){
+
+    var [time, settime] = useState( new Date().toLocaleTimeString());
+    
+    function updatetime(){
+        time = settime( new Date().toLocaleTimeString());
+    }
+    return (
+        <>
+        <h1>The current time is {time}</h1>
+        <button onClick={updatetime}>update</button>
+        </>
+    );
+}
