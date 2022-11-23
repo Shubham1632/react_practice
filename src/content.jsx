74cb817cceb74d8d92382c480e45d3db25a66994
shubham-gangurde_psl <shubham_gangurde@persistent.com>
2022-11-23 11:00:15 +0530

    implemented conditional rendering

--- src/content.jsx
+++ src/content.jsx
@@ -6,11 +6,18 @@ function makenote(note){
     return <Note key = {note.id} heading = {note.heading} description = {note.description}> </Note>
 }
 
+let completed = true;
+
+function rendercondition(){
+    return completed ? <h1>ther notes are completed</h1> : <>{notelist.map(makenote)}</>;
+
+}
+
 export default function Content(){
     return(
        <>
-       <p>these are the notes</p> 
-       {notelist.map(makenote)}
+       {rendercondition()}
+       
        </>
     );
 }
--- src/task.js
+++ src/task.js
@@ -2,27 +2,27 @@ const notelist = [
   {
     id: 1,
     heading: "lorem",
-    description: "description",
+    description: "I wanted to live deep, and suck out all the marrow of life",
   },
   {
     id: 2,
     heading: "lorem",
-    description: "lorem2",
+    description: "I wanted to live deep, and suck out all the marrow of life",
   },
   {
     id: 3,
     heading: "lorem",
-    description: "description",
+    description: "I wanted to live deep, and suck out all the marrow of life",
   },
   {
     id: 4,
     heading: "lorem",
-    description: "description",
+    description: "I wanted to live deep, and suck out all the marrow of life",
   },
   {
     id: 5,
     heading: "lorem",
-    description: "description",
+    description: "I wanted to live deep, and suck out all the marrow of life",
   },
 ];
 
